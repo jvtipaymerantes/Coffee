adff02ed7aedde5c08bb057453fb6b2c7f120f77
Brandell <brandell.reyes25@gmail.com>
2021-05-20 13:17:35 +0800

    Registration

--- coposProject/Forms/registerForm.cs
+++ coposProject/Forms/registerForm.cs
@@ -12,24 +12,30 @@ namespace coposProject
 {
     public partial class registerForm : Form
     {
+        private OleDbConnection con = new OleDbConnection();
+
         public registerForm()
         {
             InitializeComponent();
+            con.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=coposDb.accdb";
         }
 
         private void registerForm_Load(object sender, EventArgs e)
         {
-            try
-            {
-                OleDbConnection con = new OleDbConnection();
-                con.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=coposDb.accdb";
-                con.Open();
-                MessageBox.Show("Connection Success");
-                con.Close();
-            }catch(Exception ex){
-                MessageBox.Show("Connection Error!" + ex);
-            }// End of try catch
+           
 
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            OleDbCommand cmd = con.CreateCommand();
+            con.Open();
+            //cmd.CommandText = "Insert into registration(employeeID,employee_name,position,username,password,question1,question2) Values('" + txtboxid.Text + "', '" + txtboxname.Text + "', '" + cboxposition.Text + "', '" + txtboxusern.Text + "', '" + txtboxpass.Text + "', '"  + txtboxquest1.Text + "', '" + txtboxquest2.Text + "') ";
+            cmd.CommandText = " INSERT INTO registration(employeeID, employee_name, positionn, usernamee, passwordd, question1, question2) values('" + txtboxid.Text + "' , '" + txtboxname.Text + "', '" + cboxposition.Text.ToString() + "','" + txtboxusern.Text + "','" + txtboxpass.Text + "', '" + txtboxquest1.Text + "', '" + txtboxquest2.Text + "') "; 
+            cmd.Connection = con;
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Record Submitted", "Congrats");
+            con.Close();
         }
     }
 }
